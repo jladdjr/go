daafdbd21d122e43182792c58f4060a0cea399f6
Jim Ladd <jladd@redhat.com>
2020-07-28 20:47:17 -0700

    add more test scenarios for test_corner_eye
    * test all corners

--- tests/test_board_analysis.py
+++ tests/test_board_analysis.py
@@ -1,3 +1,5 @@
+import pytest
+
 from go.board import (Board, PointState)
 from go.board_analysis import BoardAnalysis  # NOQA
 
@@ -7,11 +9,17 @@ EMPTY = PointState.EMPTY
 
 
 class TestSimpleEyeDetection(object):
-    def test_corner_eye(self):
+    @pytest.mark.parametrize('points_in_eye', [[(0, 1), (1, 1), (1, 0)],
+                                               [(3, 4), (3, 3), (4, 3)],
+                                               [(0, 3), (1, 3), (1, 4)],
+                                               [(0, 3), (1, 3), (1, 4)]],
+                             ids=['bottom_left_corner_eye',
+                                  'top_right_corner_eye',
+                                  'top_left_corner_eye',
+                                  'bottom_right_corner_eye'])
+    def test_corner_eye(self, points_in_eye):
         b = Board(5)
-        points_in_eye = set([(0, 1),
-                             (1, 1),
-                             (1, 0)])
+        points_in_eye = set(points_in_eye)
         for point in points_in_eye:
             b[point] = B
 
